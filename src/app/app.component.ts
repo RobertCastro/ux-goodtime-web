cbb29c74df3ee7377188c289b07dad119c6a4275
Robert Castro <oneroberth@gmail.com>
2024-09-20 14:17:56 -0500

    Welcome into layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, LayoutComponent, RouterOutlet],
+  imports: [MatButtonModule, MatIconModule, LayoutComponent, RouterOutlet, AddressFormComponent, DashboardComponent, WelcomeComponent],
 })
 export class AppComponent {
   title = 'goodtime-web';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
   {
@@ -13,44 +12,46 @@ export const routes: Routes = [
       import('./welcome/welcome.component').then(
         (c) => c.WelcomeComponent
       ),
-    title: 'Welcome'
+      title: 'Dashboard'
   },
   {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: 'address',
-        loadComponent: () =>
-          import('./address-form/address-form.component').then(
-            (c) => c.AddressFormComponent
-          ),
-        title: 'Address'
-      },
-      {
-        path: 'table',
-        loadComponent: () =>
-          import('./table/table.component').then(
-            (c) => c.TableComponent
-          ),
-        title: 'Table'
-      },
-      {
-        path: 'tree',
-        loadComponent: () =>
-          import('./tree/tree.component').then(
-            (c) => c.TreeComponent
-          ),
-        title: 'Tree'
-      },
-      {
-        path: 'drag-drop',
-        loadComponent: () =>
-          import('./drag-drop/drag-drop.component').then(
-            (c) => c.DragDropComponent
-          ),
-        title: 'Drag-Drop'
-      }
-    ]
-  }
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+      title: 'Dashboard'
+  },
+  {
+    path: 'address',
+    loadComponent: () =>
+      import('./address-form/address-form.component').then(
+        (c) => c.AddressFormComponent
+      ),
+      title: 'Address'
+  },
+  {
+    path: 'table',
+    loadComponent: () =>
+      import('./table/table.component').then(
+        (c) => c.TableComponent
+      ),
+      title: 'Table'
+  },
+  {
+    path: 'tree',
+    loadComponent: () =>
+      import('./tree/tree.component').then(
+        (c) => c.TreeComponent
+      ),
+      title: 'Tree'
+  },
+  {
+    path: 'drag-drop',
+    loadComponent: () =>
+      import('./drag-drop/drag-drop.component').then(
+        (c) => c.DragDropComponent
+      ),
+      title: 'Drag-Drop'
+  },
 ];
