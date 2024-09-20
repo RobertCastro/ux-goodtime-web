8bebe584c9129b39af69ccf125c21227ae7c49f0
Robert Castro <oneroberth@gmail.com>
2024-09-20 13:20:05 -0500

    Navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule], // Importa el módulo de Material para botones
+  imports: [MatButtonModule, MatIconModule, LayoutComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'goodtime-web';
